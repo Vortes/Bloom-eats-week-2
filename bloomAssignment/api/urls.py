cd8aea24153de396ccf5d8271c145f5bbc90ab9c
vortes <zeyu.weng@gmail.com>
2021-11-12 11:57:27 -0500

    query users by pk

--- bloomAssignment/api/urls.py
+++ bloomAssignment/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import users
+from .views import users, users_detail
 
 urlpatterns = [
     path('users/', users),
+    path('users_detail/<int:pk>/', users_detail),
 ]
--- bloomAssignment/api/views.py
+++ bloomAssignment/api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import serializers
 from rest_framework.parsers import JSONParser
 from .models import AppUser, Tweet
 from .serializers import UserSerializer, TweetSerializer
@@ -13,3 +14,16 @@ def users(request):
         print(users)
         serializer = UserSerializer(users, many=True)
         return JsonResponse(serializer.data, safe=False)
+
+
+@csrf_exempt
+def users_detail(request, pk):
+    
+    try:
+        users = AppUser.objects.get(pk=pk)
+    except AppUser.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = UserSerializer(users)
+        return JsonResponse(serializer.data)
